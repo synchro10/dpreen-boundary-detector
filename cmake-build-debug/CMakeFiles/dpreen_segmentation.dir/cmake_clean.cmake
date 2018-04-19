e088b74e010b6880c47abc37929c09b8022efcab
synchro10 <mmvb1000@gmail.com>
2018-04-19 15:08:39 +0700

    fixed some bugs

---
+++ cmake-build-debug/CMakeFiles/dpreen_segmentation.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dpreen_segmentation.dir/Sources/main.cpp.o"
+  "CMakeFiles/dpreen_segmentation.dir/Sources/OpponentColorStage/OpponentColorStage.cpp.o"
+  "CMakeFiles/dpreen_segmentation.dir/Sources/ChromaticContourStage/ChromaticContourStage.cpp.o"
+  "CMakeFiles/dpreen_segmentation.dir/Sources/CompetitiveCooperativeStage/CompetitiveCooperativeStage.cpp.o"
+  "CMakeFiles/dpreen_segmentation.dir/Sources/Model/Model.cpp.o"
+  "CMakeFiles/dpreen_segmentation.dir/Sources/Utility/Util.cpp.o"
+  "CMakeFiles/dpreen_segmentation.dir/Sources/RegionEnhancementStage/RegionEnhancementStage.cpp.o"
+  "dpreen_segmentation.pdb"
+  "dpreen_segmentation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dpreen_segmentation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
